ab3cb9a42d01edde99cf8687c71009ae7f895d50
Dudorova Daria <154224226+radiorealm@users.noreply.github.com>
2025-06-02 14:57:40 +0300

    Fix search URL pattern and view

--- grants/urls.py
+++ grants/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('search/', views.search, name='search'),
     path('grant/<int:grant_id>/', views.detail, name='detail'),
 ]
