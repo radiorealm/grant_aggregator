72c26b256016889232121880fa80ba34a6bf7d18
Dudorova Daria <154224226+radiorealm@users.noreply.github.com>
2025-06-02 14:48:24 +0300

    Update site to Russian and improve formatting

--- grants/views.py
+++ grants/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from . import database
 
+def format_list_or_string(value):
+    if isinstance(value, (list, tuple)):
+        return ', '.join(str(item) for item in value)
+    return str(value)
+
 def index(request):
     return render(request, 'grants/index.html')
 
@@ -12,12 +17,17 @@ def search(request):
 def detail(request, grant_id):
     grant = database.get_grant_by_id(grant_id)
     if not grant:
-        return render(request, 'grants/detail.html', {'error': 'Grant not found'})
+        return render(request, 'grants/detail.html', {'error': 'Грант не найден'})
 
     keys = ['id', 'site', 'name', 'country', 'grantor', 'facts']
+    grant_dict = dict(zip(keys, grant))
+    
+    # Format country and facts if they're lists
+    grant_dict['country'] = format_list_or_string(grant_dict['country'])
+    grant_dict['facts'] = format_list_or_string(grant_dict['facts'])
+    
     query = request.GET.get('q', '')
     return render(request, 'grants/detail.html', {
-        'grant': dict(zip(keys, grant)),
+        'grant': grant_dict,
         'query': query
     })
-
